3f3ac39e4c7ea90e549a52169f2469fc80002ec4
Kieran Fowlds <kfowlds@cisco.com>
2023-08-09 17:07:52 +0100

    Add ocaml-lsp ensure install to mason-lspconfig. Add ocamlformat ensure install to mason-null-ls.

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -9,15 +9,13 @@ return {
   --     end
   --   end,
   -- },
-  -- {
-  --   "williamboman/mason-lspconfig.nvim",
-  --   opts = function(_, opts)
-  --     opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "clangd", "cssls", "html" })
-  --   end,
-  -- },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "ocamllsp" }) end,
+  },
   {
     "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "prettier") end,
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "ocamlformat") end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
