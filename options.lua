afc7e87a6a7d683911666260331b1be59e0fe572
Kieranoski702 <kfowlds1812@gmail.com>
2024-04-02 01:00:59 +0100

    Add vim tex pdf viewer

--- options.lua
+++ options.lua
@@ -18,6 +18,7 @@ return {
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     instant_username = "Kieranoski", -- set the username for instant neovim plugin
+    vimtex_view_method = "zathura", -- set the pdf viewer for vimtex
   },
 }
 -- If you need more control, you can use the function()...end notation
